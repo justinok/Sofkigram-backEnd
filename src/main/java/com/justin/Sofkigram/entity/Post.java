e31c615ec471588e14f99faa6153574f5e5d2347
justinok <38015681+justinok@users.noreply.github.com>
2022-08-08 12:12:20 -0500

    docs

--- src/main/java/com/justin/Sofkigram/entity/Post.java
+++ src/main/java/com/justin/Sofkigram/entity/Post.java
@@ -5,6 +5,18 @@ import lombok.Data;
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
+/**
+ *  The intention of this class is to connect with MySQL and mre specifically
+ *  to connect with post table inside our Schema, so we call it a entity and
+ *  wire all the columns inside
+ *
+ * @onetomany is a spring anotation to relate the two tables inside this
+ *            project and create the one post/multiple comments relationship
+ *
+ * @author Justin Diaz - jusadiazjim@unal.edu.co
+ * @since 1.0
+ * @version 3.4.1
+ */
 @Entity(name = "Post")
 @Table(name = "post")
 @Data
--- src/main/java/com/justin/Sofkigram/repository/IPostRepository.java
+++ src/main/java/com/justin/Sofkigram/repository/IPostRepository.java
@@ -3,7 +3,13 @@ package com.justin.Sofkigram.repository;
 import com.justin.Sofkigram.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+/**
+ *  The intention of this interface is to extend from the jpa repository
+ *
+ * @author Justin Diaz - jusadiazjim@unal.edu.co
+ * @since 2.0
+ * @version 3.4.1
+ */
 @Repository
 public interface IPostRepository extends JpaRepository<Post, Long> {
 }
